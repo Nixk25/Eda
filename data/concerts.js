da760ae66fa2d8c8188be4bbb07f0ec0e483c5db
Nicolas <nick2003@email.cz>
2024-02-14 18:11:14 +0100

    uprava casu

--- data/concerts.js
+++ data/concerts.js
@@ -1,15 +1,15 @@
 const concerts = [
-  { date: '2024-03-09T14:00:00+00:00', name: 'Vysoké Mýto, Mklub Odborářský ples Iveco' },
-  { date: '2024-03-16T14:00:00+00:00', name: 'Pardubice, Restaurace Rosignano Koncert společně s V.V.Ú & V.P.R.' },
-  { date: '2024-06-15T14:00:00+01:00', name: 'Újezd u Přelouče, Letní parket Újezd Punkplugged acoustic fest' },
-  { date: '2024-06-15T14:00:00+01:00', name: 'Hostinné v Podkrkonoší Tóny v parku' },
-  { date: '2024-07-13T14:00:00+01:00', name: 'Zálší u Vysokého Mýta Kocanda Fest' },
-  { date: '2024-07-27T14:00:00+01:00', name: 'Staré Město pod Sněžníkem, náměstí Anenská pouť' },
-  { date: '2024-08-03T14:00:00+01:00', name: 'Vraclav u Vysokého Mýta Vršovák Festival' },
-  { date: '2024-08-17T14:00:00+01:00', name: 'Česká Třebová restaurace Na Horách' },
-  { date: '2024-08-30T14:00:00+01:00', name: 'Seč- Ústupky SEČ SE LOUČÍ S LÉTEM – akustický set' },
-  { date: '2024-01-25T14:00:00+01:00', name: 'Vysoké Mýto M klub Buzerant + Taková normální rodinka band ' },
-  { date: '2025-09-12T14:00:00+01:00', name: 'Lanškroun, Lanškroun– náměstí Lanškrounská kopa' },
+  { date: '2024-03-09T20:00:00+01:00', name: 'Vysoké Mýto, Mklub Odborářský ples Iveco' },
+  { date: '2024-03-16T20:00:00+01:00', name: 'Pardubice, Restaurace Rosignano Koncert společně s V.V.Ú & V.P.R.' },
+  { date: '2024-06-15T20:00:00+02:00', name: 'Újezd u Přelouče, Letní parket Újezd Punkplugged acoustic fest' },
+  { date: '2024-06-15T20:00:00+02:00', name: 'Hostinné v Podkrkonoší Tóny v parku' },
+  { date: '2024-07-13T20:00:00+02:00', name: 'Zálší u Vysokého Mýta Kocanda Fest' },
+  { date: '2024-07-27T20:00:00+02:00', name: 'Staré Město pod Sněžníkem, náměstí Anenská pouť' },
+  { date: '2024-08-03T20:00:00+02:00', name: 'Vraclav u Vysokého Mýta Vršovák Festival' },
+  { date: '2024-08-17T20:00:00+02:00', name: 'Česká Třebová restaurace Na Horách' },
+  { date: '2024-08-30T20:00:00+02:00', name: 'Seč- Ústupky SEČ SE LOUČÍ S LÉTEM – akustický set' },
+  { date: '2025-01-25T20:00:00+01:00', name: 'Vysoké Mýto M klub Buzerant + Taková normální rodinka band ' },
+  { date: '2025-09-12T20:00:00+02:00', name: 'Lanškroun, Lanškroun– náměstí Lanškrounská kopa' },
 ];
 
 export default concerts;

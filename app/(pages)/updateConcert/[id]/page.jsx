47f5617b50fa0aa377113e3a322395c86a1ca3f4
Nicolas <nick2003@email.cz>
2024-03-19 22:33:34 +0100

    upraveny bugy

--- app/(pages)/updateConcert/[id]/page.jsx
+++ app/(pages)/updateConcert/[id]/page.jsx
@@ -1,22 +1,38 @@
-import React from "react";
-import EditConcert from "@/components/EditConcert";
-const getConcertById = async (id) => {
-  try {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/concerts/${id}`, {
-      cache: "no-store",
-    });
-    if (!res.ok) {
-      throw new Error("Could not find concert");
-    }
-    return await res.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
+"use client";
 
-const UpdateConcert = async ({ params }) => {
+import React, { useEffect, useState } from "react";
+import EditConcert from "@/components/EditConcert";
+
+const UpdateConcert = ({ params }) => {
   const { id } = params;
-  const { concert } = await getConcertById(id);
+  const [concert, setConcert] = useState(null);
+  console.log(concert);
+  useEffect(() => {
+    const getConcert = async () => {
+      try {
+        const res = await fetch(`/api/concerts/${id}`, {
+          cache: "no-store",
+        });
+
+        if (!res.ok) {
+          throw new Error("failed to fetch");
+        }
+
+        let data = await res.json();
+
+        setConcert(data.concert);
+      } catch (err) {
+        console.log("Error loading concert", err);
+      }
+    };
+
+    getConcert();
+  }, [id]);
+
+  if (!concert) {
+    return <div>Loading...</div>;
+  }
+
   const { name, date, time } = concert;
   return <EditConcert id={id} name={name} date={date} time={time} />;
 };

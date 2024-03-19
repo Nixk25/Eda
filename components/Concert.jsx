5a3af7246d14e052a8799e104f1dc5e4b977c8dd
Nicolas <nick2003@email.cz>
2024-03-19 23:22:39 +0100

    middleware

--- components/Concert.jsx
+++ components/Concert.jsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import RemoveBtn from "./RemoveBtn";
 import UpdateBtn from "./UpdateBtn";
-
+import { useSession } from "next-auth/react";
 const Concert = ({ name, date, time, id }) => {
+  const { status } = useSession();
   const formatDate = (date) => {
     const options = {
       year: "numeric",
@@ -15,16 +18,22 @@ const Concert = ({ name, date, time, id }) => {
   return (
     <div className="flex flex-col items-center justify-between w-full text-center group sm:flex-row ">
       <h3 className="order-2 mb-3 sm:order-1 ">{name}</h3>
-      <div className="flex order-1 gap-1 font-bold group-hover:hidden sm:oder-2">
+      <div
+        className={`flex order-1 gap-1 font-bold ${
+          status === "authenticated" ? "group-hover:hidden" : ""
+        }  sm:oder-2`}
+      >
         <div className="font-bold ">{formatDate(date)}</div>
         <span>v {time}</span>
       </div>
-      <div className="order-1 hidden gap-3 group-hover:flex sm:order-2">
-        <Link href={`/updateConcert/${id}`}>
-          <UpdateBtn />
-        </Link>
-        <RemoveBtn id={id} />
-      </div>
+      {status === "authenticated" && (
+        <div className="order-1 hidden gap-3 group-hover:flex sm:order-2">
+          <Link href={`/updateConcert/${id}`}>
+            <UpdateBtn />
+          </Link>
+          <RemoveBtn id={id} />
+        </div>
+      )}
     </div>
   );
 };

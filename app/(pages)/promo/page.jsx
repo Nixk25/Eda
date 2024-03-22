0be11380ef44baf0e798107de44e759ba790a51b
Nicolas <nick2003@email.cz>
2024-03-22 19:31:00 +0100

    pridani input listu

--- app/(pages)/promo/page.jsx
+++ app/(pages)/promo/page.jsx
@@ -2,14 +2,15 @@ import logos from "@/data/logos";
 import Image from "next/image";
 import images from "@/data/images";
 import stage from "../../../public/stage-plan.jpeg";
+import inputList from "../../../public/inputList.jpg";
 const Promo = () => {
   return (
     <section className="mt-[200px] flex justify-center items-center">
       <div className="container">
         <h2 className="pageText">Pro pořadatele </h2>
         <div>
-          <h2 className="title text-center mt-10">Stage Plán</h2>
-          <div className="w-full flex-wrap flex gap-5 justify-center items-center sm:flex-row flex-col">
+          <h2 className="mt-10 text-center title">Stage Plán</h2>
+          <div className="flex flex-col flex-wrap items-center justify-center w-full gap-5 sm:flex-row">
             <Image
               src={stage}
               alt="stage plán pro kapelu taková normální rodinka band"
@@ -17,15 +18,24 @@ const Promo = () => {
           </div>
         </div>
         <div>
+          <h2 className="mt-10 text-center title">Input list </h2>
+          <div className="flex flex-col flex-wrap items-center justify-center w-full gap-5 sm:flex-row">
+            <Image
+              src={inputList}
+              alt="input list pro kapelu taková normální rodinka band"
+            />
+          </div>
+        </div>
+        <div>
           <h2 className="title text-center my-[100px]">Promo fotky zde</h2>
-          <div className="w-full flex-wrap flex gap-5 justify-center items-center sm:flex-row flex-col">
+          <div className="flex flex-col flex-wrap items-center justify-center w-full gap-5 sm:flex-row">
             {images.map(({ src, alt }, i) => (
               <Image key={i} width={300} height={300} src={src} alt={alt} />
             ))}
           </div>
           <div>
             <h2 className="title text-center my-[100px]">Loga na promo</h2>
-            <div className="w-full flex-wrap flex gap-5 justify-center items-center sm:flex-row flex-col">
+            <div className="flex flex-col flex-wrap items-center justify-center w-full gap-5 sm:flex-row">
               {logos.map(({ src, alt }, i) => (
                 <Image
                   key={i}

2652fd03a702fc56131424557b5da32228a0c95a
Nicolas <nick2003@email.cz>
2024-04-08 22:17:37 +0200

    merch uprava

--- app/(pages)/merch/page.jsx
+++ app/(pages)/merch/page.jsx
@@ -1,9 +1,37 @@
-import merch1 from "../../../public/merch1.jpg";
-import merch2 from "../../../public/merch2.jpg";
+"use client";
+
+import { useState } from "react";
+import { merch } from "@/data/merch";
 import Image from "next/image";
+import { useEffect } from "react";
+
 const Merch = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
+  useEffect(() => {
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, []);
+
   return (
-    <section className="flex items-center justify-center mt-[120px] ">
+    <section className="flex items-center justify-center mt-[150px] ">
       <div className="container">
         <h2 className="pageText">Náš merch </h2>
         <p className="text-center sm:text-start">
@@ -11,9 +39,37 @@ const Merch = () => {
           <span className="text-lg font-bold">info@tnr-band.cz</span>
         </p>
         <div className="flex flex-wrap items-center justify-center w-full gap-10 mt-10">
-          <Image src={merch1} className="object-cover max-w-[250px]" />
-          <Image src={merch2} className="object-cover max-w-[250px] " />
+          {merch.map(({ src, alt }, i) => (
+            <div key={i} className="w-[300px] h-[300px] relative">
+              <Image
+                src={src}
+                className="object-cover w-full h-full rounded-md"
+                alt={alt}
+                onClick={() => openModal(src)}
+                placeholder="blur"
+              />
+            </div>
+          ))}
         </div>
+        {selectedImage && (
+          <div className="fixed inset-0 z-10 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+            <div className="z-20  h-[90%] p-8 ">
+              <button
+                onClick={closeModal}
+                className="absolute z-20 font-extrabold text-white top-3 right-3"
+              >
+                X
+              </button>
+              <Image
+                height={400}
+                width={400}
+                className="object-contain w-full h-full rounded-md"
+                src={selectedImage}
+                placeholder="blur"
+              />
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ data/merch.js
@@ -0,0 +1,13 @@
+import merch1 from "../public/merch1.jpg";
+import merch2 from "../public/merch2.jpg";
+
+export const merch = [
+  {
+    src: merch1,
+    alt: "merch tričko taková normální rodinka band",
+  },
+  {
+    src: merch2,
+    alt: "merch tričko taková normální rodinka band",
+  },
+];

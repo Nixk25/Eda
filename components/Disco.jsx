510ba33a1142bf59b3297f914b041be098abcb4f
Nicolas <nick2003@email.cz>
2025-01-08 16:32:33 +0100

    pridani novych pisni do disko a uprava arrow

--- components/Disco.jsx
+++ components/Disco.jsx
@@ -8,33 +8,44 @@ const Disco = () => {
   return (
     <section className="my-20">
       <div className="container">
-        <h2 className="title text-center">Nejnovější Diskografie</h2>
-        <div className="flex gap-5 w-full sm:flex-row flex-col">
+        <h2 className="text-center title">Nejnovější Diskografie</h2>
+        <div className="flex flex-col w-full gap-5 sm:flex-row">
           <Link
-            href="https://youtu.be/FCpB-ltsDYE?si=G6VJk2MQHNlBHvHl"
+            href="https://youtu.be/cZZTGzLRRnw?si=Age9CzqE1V9ID6op"
             target="_blank"
+            className="w-full "
           >
-            <div>
-              <Image
-                src={music1}
-                alt="music nejde to vejš from music group Taková normální rodinka"
-                className="rounded-md my-5"
-                placeholder="blur"
-              />
-              <h3 className="text-xl font-semibold mb-3  ">Áda</h3>
+            <div className="w-full h-full ">
+              <div className="relative w-full h-full">
+                <Image
+                  src="https://img.youtube.com/vi/cZZTGzLRRnw/maxresdefault.jpg"
+                  width={400}
+                  height={400}
+                  alt="youtube video Tatova holka from music group Taková normální rodinka"
+                  className="w-full h-full my-5 rounded-md"
+                  placeholder="blur"
+                  blurDataURL="https://img.youtube.com/vi/cZZTGzLRRnw/maxresdefault.jpg"
+                />
+              </div>
+              <h3 className="my-5 text-xl font-semibold ">Tátova holka</h3>
             </div>
           </Link>
           <Link
-            href="https://youtu.be/6OVLi9TIEpY?si=SSJ2OVv9hFf_u0Ae"
+            href="https://youtu.be/rB5KPLC6KGg?si=sMZ_VaRc0SuCN7Ya"
             target="_blank"
+            className="w-full "
           >
-            <div>
-              <Image
-                src={music2}
-                alt="music nejde to vejš from music group Taková normální rodinka"
-                className="rounded-md my-5"
-              />
-              <h3 className="text-xl font-semibold mb-3 ">Špagáta</h3>
+            <div className="w-full h-full ">
+              <div className="relative w-full h-full">
+                <Image
+                  src="https://img.youtube.com/vi/rB5KPLC6KGg/maxresdefault.jpg"
+                  alt="youtube video Grinch from music group Taková normální rodinka"
+                  width={400}
+                  height={500}
+                  className="w-full h-full my-5 rounded-md"
+                />
+              </div>
+              <h3 className="my-3 text-xl font-semibold ">Grinch</h3>
             </div>
           </Link>
         </div>

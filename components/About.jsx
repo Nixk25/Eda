9e2ad1e6d4c90c77c033152b58f1e255f0c89818
Nicolas <nick2003@email.cz>
2026-06-24 23:45:33 +0200

    pridani novych fotek

--- components/About.jsx
+++ components/About.jsx
@@ -7,12 +7,6 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import photo1 from "../public/photo1.webp";
-import photo2 from "../public/photo2.webp";
-import photo3 from "../public/photo3.webp";
-import photo4 from "../public/photo4.jpg";
-import photo5 from "../public/photo5.jpg";
-import photo6 from "../public/photo6.jpg";
 import images from "@/data/images";
 
 const About = () => {
--- data/images.js
+++ data/images.js
@@ -1,9 +1,8 @@
 import photo1 from "../public/photo1.webp";
 import photo2 from "../public/photo2.webp";
 import photo3 from "../public/photo3.webp";
-import photo4 from "../public/photo4.jpg";
-import photo5 from "../public/photo5.jpg";
-import photo6 from "../public/photo6.jpg";
+import photo4 from "../public/photo4.webp";
+
 
 const images = [
   {
@@ -20,14 +19,6 @@ const images = [
   },
   {
     src: photo4,
-    alt: "fotka taková normální rodinka band",
-  },
-  {
-    src: photo5,
-    alt: "fotka taková normální rodinka band",
-  },
-  {
-    src: photo6,
     alt: "fotka taková normální rodinka band",
   },
 ];
